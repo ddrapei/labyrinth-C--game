0be402e7e6bb27d8c2e6e5709257d369f6ae77f5
d.drapei <d.drapei@rgu.ac.uk>
2025-11-13 09:27:52 +0000

    fixed warnings about null reference in PlayerEquippedDisplay class

--- CM2115-25-26-Assessment/PlayerDisplay/PlayerEquippedDisplay.cs
+++ CM2115-25-26-Assessment/PlayerDisplay/PlayerEquippedDisplay.cs
@@ -17,9 +17,9 @@ public class PlayerEquippedDisplay : IPlayerDisplay
         Console.WriteLine("=== Equipped ===".Pastel("#00ff00"));
         Console.WriteLine("Weapon: " + (player.WeaponEquiped?.Name ?? "None").Pastel("#fc030f"));
         Console.WriteLine("Shield: : " + (player.ShieldEquipped?.Name ?? "None").Pastel("#1900ff"));
-        Console.WriteLine("Head Armour: " + (((Item)player.HeadArmourEquipped)?.Name ?? "None").Pastel("#693a01"));
-        Console.WriteLine("Torso Armour: " + (((Item)player.TorsoArmourEquipped)?.Name ?? "None").Pastel("#693a01"));
-        Console.WriteLine("Legs Armour: " + (((Item)player.LegsArmourEquipped)?.Name ?? "None").Pastel("#693a01"));
+        Console.WriteLine("Head Armour: " + ((player.HeadArmourEquipped as Item)?.Name ?? "None").Pastel("#693a01"));
+        Console.WriteLine("Torso Armour: " + ((player.TorsoArmourEquipped as Item)?.Name ?? "None").Pastel("#693a01"));
+        Console.WriteLine("Legs Armour: " + ((player.LegsArmourEquipped as Item)?.Name ?? "None").Pastel("#693a01"));
         Console.WriteLine("Active Set: " + (ArmourSetManager.GetInstance().ActiveSets.Count > 0
             ? ArmourSetManager.GetInstance().ActiveSets.First().Key
             : "None").Pastel("#00e5ff"));
