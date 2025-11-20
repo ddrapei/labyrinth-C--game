cd70ae5936aa0e95e7cf93c9bd01fb49a7bc165a
d.drapei <d.drapei@rgu.ac.uk>
2025-11-20 18:06:18 +0000

    2 new tests for equipping a shield and a weapon. Orginised tests in a folder.

---
+++ CM2115-25-26-Assessment-Tests/EquipmentTests.cs
@@ -0,0 +1,63 @@
+using Items;
+using PlayerEquipment;
+
+[Collection("Sequential")]
+public class EquipmentTests
+{
+    [Fact]
+    public void EquipWeapon_DamageIncreasedWeaponEquipped()
+    {
+        // arrange
+        Player player = Player.GetInstance();
+        player.WeaponEquiped = null;
+        player.BaseAttackPower = 1;
+        player.AttackPower = 1;
+        player.Defense = 0;
+        
+        IEquipBehavior weaponBehavior = new EquipWeaponBehavior();
+        player.RegisterEquipBehavior("weapon", weaponBehavior);
+        
+        Weapon rusty_sword = new Weapon("Rusty Sword", 3);
+        player.Inventory.AddItem(rusty_sword);
+        
+        int initialInventoryCount = player.Inventory.GetInventoryCount();
+
+        // act
+        bool result = weaponBehavior.Equip(player, rusty_sword);
+
+        // assert
+        Assert.True(result);
+        Assert.Equal(4, player.AttackPower);
+        Assert.Equal(rusty_sword, player.WeaponEquiped);
+        Assert.Equal(0, player.Defense);
+        Assert.Equal(player.Inventory.GetInventoryCount(), initialInventoryCount);
+    }
+
+    [Fact]
+    public void EquipShield_DefenseIncreasedShieldEquipped()
+    {
+        // arrange
+        Player player = Player.GetInstance();
+        player.ShieldEquipped = null;
+        player.BlockingDamageChance = 0.0;
+        
+        IEquipBehavior shieldBehavior = new EquipShieldBehavior();
+        player.RegisterEquipBehavior("shield", shieldBehavior);
+        
+        Shield leather_shield = new Shield("Leather Shield", 0.15);
+        player.Inventory.AddItem(leather_shield);
+        
+        int initialInventoryCount = player.Inventory.GetInventoryCount();
+
+        // act
+        bool result = shieldBehavior.Equip(player, leather_shield);
+
+        // assert
+        Assert.True(result);
+        Assert.Equal(0.15, player.BlockingDamageChance);
+        Assert.Equal(leather_shield, player.ShieldEquipped);
+        Assert.Equal(player.Inventory.GetInventoryCount(), initialInventoryCount);
+    }
+
+
+}
