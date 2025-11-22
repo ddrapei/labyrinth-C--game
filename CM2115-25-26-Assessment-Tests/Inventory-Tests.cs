f36fa034bbf70c2a407ef9e53bf238ba72144656
d.drapei <d.drapei@rgu.ac.uk>
2025-11-22 10:33:04 +0000

    one more theory inventory test

--- CM2115-25-26-Assessment-Tests/Inventory-Tests.cs
+++ CM2115-25-26-Assessment-Tests/Inventory-Tests.cs
@@ -24,4 +24,52 @@ public class InventoryTests
         // assert
         Assert.Equal(IsFull, inventory.isFull());
     }
+
+    [Theory]
+    [InlineData(1, true)]
+    [InlineData(2, true)]
+    [InlineData(3, true)]
+    [InlineData(4, true)]
+    [InlineData(5, true)]
+    [InlineData(6, false)]
+    [InlineData(7, false)]
+    [InlineData(8, false)]
+    [InlineData(0, false)]
+    [InlineData(-1, false)]
+    [InlineData(-2, false)]
+    [InlineData(-200, false)]
+    [InlineData(200, false)]
+
+    public void GetItemByNumber_ReturnsItemByNumberIfThatItemExistsInTheInventory(int itemIndex, bool itemExistInInventory)
+    {
+        // arrange
+        Inventory inventory = new Inventory(10);
+        Weapon test_weapon = new Weapon("Weapon", 1);
+
+        inventory.AddItem(test_weapon);
+        inventory.AddItem(test_weapon);
+        inventory.AddItem(test_weapon);
+        inventory.AddItem(test_weapon);
+        inventory.AddItem(test_weapon);
+
+        // act
+        var result = inventory.GetItemByNumber(itemIndex);
+
+        // assert
+
+        if (itemExistInInventory)
+        {
+            Assert.Equal(test_weapon, result);
+        }
+        else
+        {
+            Assert.Null(result);
+        }
+
+
+
+
+
+
+    }
 }    
